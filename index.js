0dfead0e7a6b88c93599659a0366c00fa1fcfd70
jess-isaacs <jessie.isaacs14@gmail.com>
2025-01-30 21:32:15 -0600

    removed a comment

--- index.js
+++ index.js
@@ -85,21 +85,3 @@ function showMessage(fruit) {
   
   };
   
-  /* console.log(`fruit: ${fruit} ${fruitEmojis[fruit] || ''}`);
-  
-  switch (fruit) {
-    case 'orange':
-    console.log('citrus confirmed 🍊');
-    break;
-    case 'grapefruit':
-    console.log('citrus confirmed 🍊');
-    break;
-    case 'apple':
-    console.log('not citrus 🍏');
-    break;
-    default: console.log('unclear if citrus ❓🍊❓');
-  }
-*/
-  // need a function to shake tree image
-  // need a function to drop fruit image from tree to basket
-
